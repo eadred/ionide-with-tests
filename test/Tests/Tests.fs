c6f65e455c3a80bb837f6402c4f6636035926015
Eadred <eadred.birchenough@zuhlke.com>
2017-01-03 12:20:52 +0000

    Simpler integer generators

--- test/Tests/Tests.fs
+++ test/Tests/Tests.fs
@@ -7,8 +7,8 @@ open Source
 
 let listArb = Arb.generate<int> |> Gen.listOf |> Gen.filter (fun ls -> List.length ls > 0) |> Arb.fromGen
 
-let bigIntGen = Arb.generate<int> |> Gen.filter (fun x -> x > 10)
-let smallIntGen = Arb.generate<int> |> Gen.filter (fun x -> x < 10)
+let bigIntGen = Gen.choose(11, 20)
+let smallIntGen = Gen.choose(0, 9)
 
 let intPairs = Gen.map2 (fun x y -> (x,y)) bigIntGen smallIntGen |> Arb.fromGen
 
