a35d9b5bb4bbfee050d52b01e25e8ec2f519cee1
Eadred <eadred.birchenough@zuhlke.com>
2017-01-03 11:48:31 +0000

    Using Expecto.FsCheck with clunky way of using a custom Arbitrary in the test

--- test/Tests/Tests.fs
+++ test/Tests/Tests.fs
@@ -1,7 +1,16 @@
 module Tests
 
 open Expecto
+open ExpectoFsCheck
+open FsCheck
 open Source
+
+let listArb = Arb.generate<int> |> Gen.listOf |> Gen.filter (fun ls -> List.length ls > 0) |> Arb.fromGen
+
+type MyGenerators =
+  static member List() = listArb
+
+let config = { FsCheck.Config.Default with Arbitrary = (typeof<MyGenerators> :: FsCheck.Config.Default.Arbitrary)}
 
 [<Tests>]
 let tests =
@@ -9,4 +18,11 @@ let tests =
     testCase "X should return F#" <| fun _ ->
       let target = new Source.Source()
       Expect.equal target.X "F#" "X result was not F#"
+    
+    testProperty "Addition is commutative" <| fun a b ->
+      a + b = b + a
+
+    testPropertyWithConfig config "List length" <| fun (a: int list) ->
+      List.length a > 0
+
   ]
