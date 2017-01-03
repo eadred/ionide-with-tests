54ba552b7d92f9ad75033ca149e31e844b3935e7
Eadred <eadred.birchenough@zuhlke.com>
2017-01-03 11:58:28 +0000

    Less clunky way of using a custom Arbirtrary in a test

--- test/Tests/Tests.fs
+++ test/Tests/Tests.fs
@@ -7,11 +7,6 @@ open Source
 
 let listArb = Arb.generate<int> |> Gen.listOf |> Gen.filter (fun ls -> List.length ls > 0) |> Arb.fromGen
 
-type MyGenerators =
-  static member List() = listArb
-
-let config = { FsCheck.Config.Default with Arbitrary = (typeof<MyGenerators> :: FsCheck.Config.Default.Arbitrary)}
-
 [<Tests>]
 let tests =
   testList "samples" [
@@ -22,7 +17,5 @@ let tests =
     testProperty "Addition is commutative" <| fun a b ->
       a + b = b + a
 
-    testPropertyWithConfig config "List length" <| fun (a: int list) ->
-      List.length a > 0
-
+    (fun a -> List.length a > 0) |> FsCheck.Prop.forAll listArb |> testProperty "List length"
   ]
